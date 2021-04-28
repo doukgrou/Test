c910824f29ea9e96b95967e5a53f27ce9aa6dfc2
JulioLu <77286926+JulioLu@users.noreply.github.com>
2021-04-28 11:39:04 +0300

    Add files via upload

--- VAcCure/server.js
+++ VAcCure/server.js
@@ -8,15 +8,20 @@ app.use(
   })
 );
 app.use(bodyParser.json());// DB Config
-const db = require("./config/keys").mongoURI;// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db,{ useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));// Passport middleware
-app.use(passport.initialize());// Passport config
-require("./config/passport")(passport);// Routes
-app.use("/api/users", users);const port = process.env.PORT || 5000;
+  .catch(err => console.log(err));
+  
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
