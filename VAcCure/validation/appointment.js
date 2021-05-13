d59030fc3b28981bfcb018b29ebc4d5b2207685b
JulioLu <77286926+JulioLu@users.noreply.github.com>
2021-05-13 20:32:46 +0300

    Update appointment.js

--- VAcCure/validation/appointment.js
+++ VAcCure/validation/appointment.js
@@ -7,18 +7,14 @@ module.exports = function validateAppointmentInput(data) {
     // Convert empty fields toan  empty string so we can use validator functions
     data.firstName = !isEmpty(data.firstName) ? data.firstName : "";
     data.lastName = !isEmpty(data.lastName) ? data.lastName : "";
-    data.gender = !isEmpty(data.gender) ? data.gender : "";
+    //data.gender = !isEmpty(data.gender) ? data.gender : "";
     data.amka = !isEmpty(data.amka) ? data.amka : "";
     data.dateOfBirth = !isEmpty(data.dateOfBirth) ? data.dateOfBirth : "";
-    data.phone = !isEmpty(data.phone) ? data.phone : "";
+    //data.phone = !isEmpty(data.phone) ? data.phone : "";
     data.address = !isEmpty(data.address) ? data.address : "";
     data.dateDose1 = !isEmpty(data.dateDose1) ? data.dateDose1 : "";
     data.dateDose2 = !isEmpty(data.dateDose2) ? data.dateDose2 : "";
-    //data.dose1Completed = !isEmpty(data.dose1Completed) ? data.dose1Completed : "";
-    //data.dose2Completed = !isEmpty(data.dose2Completed) ? data.dose2Completed : "";
-    data.vaccineBrand = !isEmpty(data.vaccineBrand) ? data.vaccineBrand : "";
-    data.symptoms = !isEmpty(data.symptoms) ? data.symptoms : "";
-    data.comments = !isEmpty(data.comments) ? data.comments : "";
+    data.vaccineBrand = !isEmpty(data.vaccineBrand) ? data.vaccineBrand : ""
     //data.stage = !isEmpty(data.stage) ? data.stage : "";
 
     // Name checks
@@ -31,14 +27,17 @@ module.exports = function validateAppointmentInput(data) {
         errors.lastName = "Last name field is required";
     }
 
-    // Gender checks
-    if (Validator.isEmpty(data.gender)) {
-        errors.gender = "Gender field is required";
-    }
+    // // Gender checks
+    // if (Validator.isEmpty(data.gender)) {
+    //     errors.gender = "Gender field is required";
+    // }
 
     // AMKA checks
-    if (Validator.isEmpty(data.amka)) {
+    if(Validator.isEmpty(data.amka)) {
         errors.amka = "AMKA field is required";
+    }
+    if(isNaN(data.amka)) {
+        errors.amka = "AMKA field must be a number";
     }
 
     // Birthday checks
@@ -46,15 +45,28 @@ module.exports = function validateAppointmentInput(data) {
         errors.dateOfBirth = "Birthday field is required";
     }
     
-    // Phone checks
-    if (Validator.isEmpty(data.phone)) {
-        errors.phone = "Phone field is required";
+    // // Phone checks
+    // if (Validator.isEmpty(data.phone)) {
+    //     errors.phone = "Phone field is required";
+    // }
+
+    //Doses checks
+    if (Validator.isEmpty(data.dateDose1)) {
+        errors.dateDose1 = "Date of dose one field is required";
+    }
+    if (Validator.isEmpty(data.dateDose1)) {
+        errors.dateDose1 = "Date of dose two field is required";
     }
 
     //Address checks
     if (Validator.isEmpty(data.address)) {
         errors.address = "Address field is required";
     }
+
+    // // Vaccine Brand checks
+    // if (Validator.isEmpty(data.vaccineBrand)) {
+    //     errors.vaccineBrand = "Vaccine brand field is required";
+    // }
 
     // // Stage checks
     // if (Validator.isEmpty(data.stage)) {
