55f2f0ccd852e9740d01146f7e8ae28ae5237807
JulioLu <77286926+JulioLu@users.noreply.github.com>
2021-05-13 21:11:54 +0300

    Update newAppointment.js

--- VAcCure/client/src/components/vaccine/newAppointment.js
+++ VAcCure/client/src/components/vaccine/newAppointment.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { newAppointment } from "../../actions/authActions";
+import { newAppointment } from "../../actions/appointmentActions";
 import classnames from "classnames";
 
 class NewAppointment extends Component {
